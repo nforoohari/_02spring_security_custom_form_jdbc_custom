f5c91ddcd3b06319a5288ba31353c0300d71dd95
Nima Foroohari <nforoohari@gmail.com>
2025-05-22 12:59:19 +0330

    Version 1

--- src/main/java/ir/digixo/config/SecurityConfig.java
+++ src/main/java/ir/digixo/config/SecurityConfig.java
@@ -4,12 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -19,7 +16,6 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class SecurityConfig {
 
-
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
@@ -27,26 +23,23 @@ public class SecurityConfig {
         jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
         return jdbcUserDetailsManager;
     }
-/*
+
     @Bean
-    public PasswordEncoder passwordEncoder()
-    {
-        return  NoOpPasswordEncoder.getInstance();
-    }*/
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-
-    ///
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
-                authorizationManagerRequestMatcherRegistry -> {
-                    authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
-                }
-        )
+                        authorizationManagerRequestMatcherRegistry -> {
+                            authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
+                        }
+                )
                 .formLogin(httpSecurityFormLoginConfigurer -> {
                     httpSecurityFormLoginConfigurer
-                            .loginPage("/showMyLoginform")
-                            .loginProcessingUrl("/login2")
+                            .loginPage("/showMyLoginForm")
+                            .loginProcessingUrl("/loginProcess")
                             .permitAll();
                 })
                 .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.permitAll())
